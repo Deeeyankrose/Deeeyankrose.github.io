05b6b03c25e495fdb4d336357c22dc68864b4a0b
unknown <deeyank@gmail.com>
2023-01-30 21:26:21 +0700

    initial commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-    $("button").click(function(){
-     alert($("#abb").attr("href"));
+    $("button1").click(function(){
+     alert($("#abb").attr("href1"));
     });
    });
